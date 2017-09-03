6d48e2bde8cda2281fc09be97f43a774092697c2
Jason <jcsummers04@gmail.com>
2017-09-02 17:27:39 -0700

    Updating work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-	var fb = new Firebase("https://itsatrain.firebaseio.com/");
+	var fb = new Firebase("https://trainscheduler-a8f90.firebaseio.com");
+
+	// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA66ZRD-UW9Mx2k0RhmRNfv7h6ZeuCUcVY",
+    authDomain: "trainscheduler-a8f90.firebaseapp.com",
+    //databaseURL: "https://trainscheduler-a8f90.firebaseio.com",
+    projectId: "trainscheduler-a8f90",
+    storageBucket: "trainscheduler-a8f90.appspot.com",
+    messagingSenderId: "779699792067"
+  };
+
+  firebase.initializeApp(config);
+
 
 	//Variables for the initial case, will hold user data
 	var name = "";
